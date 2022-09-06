2483b48685ccdac2b612d963d23284892f76068b
masachika <masachika.kmd@gmail.com>
2022-09-06 15:56:52 +0900

    データの取得と結果の書き出しまで実装

---
+++ main.py
@@ -0,0 +1,78 @@
+import time
+import datetime
+import threading
+import sys
+import PySimpleGUI as sg
+import win32gui
+import win32process
+import psutil
+import csv
+from gui import create_layout
+
+
+# スレッド処理のクラス
+class Recorder():
+    def __init__(self):  # 初期化
+        self.ROOP = False  # ループのフラグ
+
+    # ループ処理関数
+    def __target(self):
+        prev = ""
+        fname = datetime.datetime.now().strftime("timane-%Y%m%d.csv")
+        while self.ROOP:
+            win_name = win32gui.GetWindowText(win32gui.GetForegroundWindow())
+            if win_name != prev:
+                prev = win_name
+                pid = win32process.GetWindowThreadProcessId(win32gui.GetForegroundWindow())
+                exe_name = psutil.Process(pid[-1]).name()
+                print(win_name, exe_name)
+                datetime.datetime.now().strftime("%Y%m%d-%H:%M:%S")
+                with open(fname, mode="a", newline="", encoding="shift-jis", errors="ignore") as f:
+                    writer = csv.writer(f)
+                    now = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+                    writer.writerow([now, win_name, exe_name])
+            time.sleep(self.INTERVAL)
+
+    # スレッドをスタートさせる
+    def __start(self):
+        if threading.active_count() == 1:
+            self.thread = threading.Thread(target=self.__target)
+            self.thread.start()
+
+    def startEvent(self, interval):  # STARTボタン押下時の処理
+        print("=== START ===")
+        self.ROOP = True
+        self.INTERVAL = int(interval)
+        self.__start()
+
+    def finishEvent(self):  # STOPボタン押下処理
+        print("=== STOP ===")
+        self.ROOP = False  # ループ停止->自動的にスレッド破棄
+
+
+def main():
+    # スレッド処理のインスタンス生成
+    r = Recorder()
+
+    # ウインドウの表示、設定
+    window = sg.Window("タイマネ", create_layout(), finalize=True)
+
+    while True:
+
+        event, values = window.read()
+
+        # ボタンの処理内容
+        if event == "START":
+            r.startEvent(values["interval"])
+
+        elif event == "STOP":
+            r.finishEvent()
+
+        elif event is None:
+            r.finishEvent()
+            window.close()
+            sys.exit()
+
+
+if __name__ == "__main__":
+    main()
